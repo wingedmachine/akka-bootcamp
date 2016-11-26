b9c5a4411f80511a9007def379c826e9df3db70a
Wheeler <wheeler.pryor@gmail.com>
2016-11-26 17:01:24 -0500

    created ValidationActor

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -47,42 +47,42 @@ namespace WinTail
         private void GetAndValidateInput()
         {
             var message = Console.ReadLine();
-            if (string.IsNullOrEmpty(message))
-            {
-                // signal that the user needs to supply non-blank input
-                Self.Tell(new Messages.NullInputError("No input received."));
-            }
-            else if (String.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
-            {
-                // shut it all down
-                Context.System.Terminate();
-            }
-            else
-            {
-                var valid = IsValid(message);
-                if (valid)
-                {
-                    _consoleWriterActor.Tell(new Messages.InputSuccess("You did the thing!"));
+            //if (string.IsNullOrEmpty(message))
+            //{
+            //    // signal that the user needs to supply non-blank input
+            //    Self.Tell(new Messages.NullInputError("No input received."));
+            //}
+            //else if (String.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            //{
+            //    // shut it all down
+            //    Context.System.Terminate();
+            //}
+            //else
+            //{
+            //    var valid = IsValid(message);
+            //    if (valid)
+            //    {
+            //        _consoleWriterActor.Tell(new Messages.InputSuccess("You did the thing!"));
 
-                    //continue reading from console
-                    Self.Tell(new Messages.ContinueProcessing());
-                }
-                else
-                {
-                    Self.Tell(new Messages.ValidationError("An odd number of chars? Fuck you."));
-                }
-            }
+            //        //continue reading from console
+            //        Self.Tell(new Messages.ContinueProcessing());
+            //    }
+            //    else
+            //    {
+            //        Self.Tell(new Messages.ValidationError("An odd number of chars? Fuck you."));
+            //    }
+            //}
         }
 
-        /// <summary>
-        /// Validates <see cref="message"/> 
-        /// </summary>
-        /// <param name="message"></param>
-        /// <returns></returns>
-        private static bool IsValid(string message)
-        {
-            return message.Length % 2 == 0;
-        }
+        ///// <summary>
+        ///// Validates <see cref="message"/> 
+        ///// </summary>
+        ///// <param name="message"></param>
+        ///// <returns></returns>
+        //private static bool IsValid(string message)
+        //{
+        //    return message.Length % 2 == 0;
+        //}
         #endregion
     }
 }
--- src/Unit-1/DoThis/Messages.cs
+++ src/Unit-1/DoThis/Messages.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Akka;
-
-namespace WinTail
+﻿namespace WinTail
 {
     class Messages
     {
