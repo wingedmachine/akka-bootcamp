d5cb354b55824e13abe887e571d1e1f88f9c7870
Wheeler <wheeler.pryor@gmail.com>
2016-11-28 18:03:33 -0500

    created PerformanceCounterActor

---
+++ src/Unit-2/DoThis/Actors/PerformanceCounterActor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using Akka.Actor;
+
+namespace ChartApp.Actors
+{
+    public class PerformanceCounterActor : UntypedActor
+    {
+        private readonly string _seriesName;
+        private readonly Func<PerformanceCounter> _performanceCounterGenerator;
+        private PerformanceCounter _counter;
+
+        private readonly HashSet<IActorRef> _subscriptions;
+        private readonly ICancelable _cancelPublishing;
+
+        public PerformanceCounterActor(string seriesName, Func<PerformanceCounter> performanceCounterGenerator)
+        {
+            _seriesName = seriesName;
+            _performanceCounterGenerator = performanceCounterGenerator;
+            _subscriptions = new HashSet<IActorRef>();
+            _cancelPublishing = new Cancelable(Context.System.Scheduler);
+        }
+
+        #region Actor lifecycle methods
+
+        protected override void PreStart() //wert: change to ScheduleTellRepeatedlyCancelable
+        {
+            _counter = _performanceCounterGenerator();
+            Context.System.Scheduler.ScheduleTellRepeatedly(
+                TimeSpan.FromMilliseconds(250),
+                TimeSpan.FromMilliseconds(250),
+                Self,
+                new GatherMetrics(),
+                Self,
+                _cancelPublishing);
+        }
+
+        protected override void PostStop()
+        {
+            try
+            {
+                _cancelPublishing.Cancel(false);
+                _counter.Dispose();
+            }
+            catch { }
+            finally
+            {
+                base.PostStop();
+            }
+        }
+
+        #endregion
+
+        protected override void OnReceive(object message)
+        {
+            if (message is GatherMetrics)
+            {
+                var metric = new Metric(_seriesName, _counter.NextValue());
+                foreach (var sub in _subscriptions)
+                {
+                    sub.Tell(metric);
+                }
+            }
+            else if (message is SubscribeCounter)
+            {
+                var sc = message as SubscribeCounter;
+                _subscriptions.Add(sc.Subscriber);
+            }
+            else if (message is UnsubscribeCounter)
+            {
+                var uc = message as UnsubscribeCounter;
+                _subscriptions.Remove(uc.Subscriber);
+            }
+        }
+    }
+}
