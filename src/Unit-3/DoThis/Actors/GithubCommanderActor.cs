bcd9f6da568e3c91c723e6ffb9b9c5476c210c83
Wheeler <wheeler.pryor@gmail.com>
2016-12-02 11:19:52 -0500

    fix mistake in PreRestart
    and remove bookmark comment

--- src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
+++ src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
@@ -68,7 +68,7 @@ namespace GithubActors.Actors
         private void BecomeAsking()
         {
             _canAcceptJobSender = Sender;
-            _pendingJobReplies = _coordinator.Ask<Routees>(new GetRoutees()).Result.Members.Count(); //wert
+            _pendingJobReplies = _coordinator.Ask<Routees>(new GetRoutees()).Result.Members.Count();
             Become(Asking);
         }
 
@@ -116,12 +116,8 @@ namespace GithubActors.Actors
 
         protected override void PreRestart(Exception reason, object message)
         {
-            var c1 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
-                ActorPaths.GithubCoordinatorActor.Name + "1");
-            var c2 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
-                ActorPaths.GithubCoordinatorActor.Name + "2");
-            var c3 = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()),
-                ActorPaths.GithubCoordinatorActor.Name + "3");
+            _coordinator.Tell(PoisonPill.Instance);
+            base.PreRestart(reason, message);
         }
     }
 }
